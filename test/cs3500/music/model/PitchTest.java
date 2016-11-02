66dfa55f06bc7ebd7679158e97167b9088e6cf3e
carneyrob <robert.edward.carney@gmail.com>
2016-11-02 14:18:30 -0400

    Update PitchTest.java

--- test/cs3500/music/model/PitchTest.java
+++ test/cs3500/music/model/PitchTest.java
@@ -5,28 +5,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by tiger on 10/31/16.
+ * For the purposes of testing the Pitch class.
  */
 public class PitchTest {
+  Pitch pitch0 = new Pitch(5);
+  Pitch pitch1 = new Pitch(5);
+  Pitch pitch2 = new Pitch(6);
 
   @Test
   public void testEquals() throws Exception {
-
+    assertEquals(true, pitch0.equals(pitch1));
+    assertEquals(false, pitch0.equals(pitch2));
   }
 
   @Test
   public void testHashCode() throws Exception {
-
+    assertEquals(true, pitch1.hashCode() == pitch0.hashCode());
   }
 
-  @Test
+  @Test(expected = IllegalArgumentException.class)
   public void testAdd() throws Exception {
-
+    pitch0.add(null);
   }
 
-  @Test
+  @Test(expected = IllegalArgumentException.class)
   public void testRemove() throws Exception {
-
+    pitch0.remove(null);
   }
 
 }
