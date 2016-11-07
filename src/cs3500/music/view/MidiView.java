1f6a7b93219cc6be4e95492134cedcf7e860d0be
carneyrob <robert.edward.carney@gmail.com>
2016-11-06 20:06:55 -0500

    Made midi IView

--- src/cs3500/music/view/MidiView.java
+++ src/cs3500/music/view/MidiView.java
@@ -5,7 +5,7 @@ import javax.sound.midi.InvalidMidiDataException;
 /**
  * Created by tiger on 10/31/16.
  */
-public interface MidiView {
+public interface MidiView extends IView {
 
   public void playNote() throws InvalidMidiDataException;
 }
