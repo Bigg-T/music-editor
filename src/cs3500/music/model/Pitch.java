4ad5c06aa59966fd1653013544aa800fdd9ab851
carneyrob <robert.edward.carney@gmail.com>
2016-11-07 20:34:48 -0500

    Style Fixes

--- src/cs3500/music/model/Pitch.java
+++ src/cs3500/music/model/Pitch.java
@@ -1,14 +1,11 @@
 package cs3500.music.model;
 
 import cs3500.music.util.Utils;
-import javafx.collections.transformation.SortedList;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.OptionalInt;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * Created by tiger on 11/1/16.
@@ -47,11 +44,9 @@ class Pitch {
   /**
    * INVARIANT: The noteList will be be sorted by pitch of the note in ascending order.
    * The list of note will not contains any identical notes.
-   * <p>
-   * EFFECT: The position will change based on their pitch to be in order.
-   * <p>
-   * Return true if the note is added. Return false if the note is already in the list and
-   * will not be added. Throw an exception if the note is null.
+   * <p>EFFECT: The position will change based on their pitch to be in order.</p>
+   * <p>Return true if the note is added. Return false if the note is already in the list and
+   * will not be added. Throw an exception if the note is null.</p>
    *
    * @param note the note to be added.
    * @return return true if the note is added
