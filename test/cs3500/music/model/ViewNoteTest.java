2b189ff28a2381c77978a10e0fbeb56e0d710f25
carneyrob <robert.edward.carney@gmail.com>
2016-11-07 19:00:54 -0500

    Added viewnotetest

--- test/cs3500/music/model/ViewNoteTest.java
+++ test/cs3500/music/model/ViewNoteTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by tiger on 11/3/16.
+ * For the purposes of testing ViewNote.
  */
 public class ViewNoteTest {
   private Note note0 = new NoteBuilder().setNoteName(NoteName.C).setOctave(4).setStartDuration(2).
           setNumBeats(2).setChannel(0).setVolume(5).buildNote();
+  private ViewNote viewNote0 = NoteBuilder.buildNote(note0);
+
 
   //Note is not ViewNote
   @Test
@@ -24,5 +26,29 @@ public class ViewNoteTest {
     Assert.assertEquals(true, NoteBuilder.buildNote(note0).isViewNote());
   }
 
+  @Test
+  public void testGetNoteName()  {
+    Assert.assertEquals(NoteName.C, viewNote0.getNoteName());
+  }
+
+  @Test
+  public void testBeat()  {
+    Assert.assertEquals(2, viewNote0.getBeat());
+  }
+
+  @Test
+  public void testStartDuration()  {
+    Assert.assertEquals(2, viewNote0.getStartDuration());
+  }
+
+  @Test
+  public void testChannel()  {
+    Assert.assertEquals(0, viewNote0.getChannel());
+  }
+
+  @Test
+  public void testVolume()  {
+    Assert.assertEquals(0, viewNote0.getVolume());
+  }
 
 }
