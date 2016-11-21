3c11d8d348f249596a1c7953ab28b7d4a3166805
carneyrob <robert.edward.carney@gmail.com>
2016-11-21 18:47:18 -0500

    Add files via upload

---
+++ src/cs3500/music/controller/PlayAction.java
@@ -0,0 +1,28 @@
+package cs3500.music.controller;
+
+import cs3500.music.view.IView;
+
+/**
+ * For the purposes of playing the view.
+ */
+public class PlayAction implements Runnable {
+
+  /**
+   * View to be used here.
+   */
+  IView theView;
+
+  /**
+   * Creates a play action.
+   * @param theView view to use
+   */
+  public PlayAction(IView theView) {
+    this.theView = theView;
+  }
+
+
+  @Override
+  public void run() {
+    theView.resume();
+  }
+}
