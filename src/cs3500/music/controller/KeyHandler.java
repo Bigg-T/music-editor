732393b13cea293ea8243233ae3baea5115f20f6
carneyrob <robert.edward.carney@gmail.com>
2016-11-21 15:41:23 -0500

    KH Style

--- src/cs3500/music/controller/KeyHandler.java
+++ src/cs3500/music/controller/KeyHandler.java
@@ -1,6 +1,8 @@
 package cs3500.music.controller;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
