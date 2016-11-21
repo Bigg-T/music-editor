cb7143fb8d41dc60de55edbd495f4f1c10b4efe8
carneyrob <robert.edward.carney@gmail.com>
2016-11-21 13:03:14 -0500

    Small IView update

--- src/cs3500/music/view/IView.java
+++ src/cs3500/music/view/IView.java
@@ -1,6 +1,7 @@
 package cs3500.music.view;
 
 import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 
 import cs3500.music.model.IBasicMusicEditor;
 import cs3500.music.model.INote;
@@ -36,19 +37,53 @@ public interface IView {
    */
   public void addKeyListener(KeyListener keyListener);
 
+  /**
+   * Adds the key listener if possible.
+   * @param mouseListener the mouse listener to be used.
+   */
+  public void addMouseListener(MouseListener mouseListener);
+
+  /**
+   * Gets the current tick of the view, if possible.
+   * @return
+   */
   public long getCurrentTick();
 
+  /**
+   * Jumps to beginning, if applicable.
+   */
   public void jumpToBeginning();
 
+  /**
+   * Jumps to end, if possible.
+   */
   public void jumpToEnd();
 
+  /**
+   * Moves to tick location.
+   * @param tick tick location
+   */
   public void move(long tick);
 
+  /**
+   * Pauses view, if applicable.
+   */
   public void pause();
 
+  /**
+   * Resumes view, if applicable.
+   */
   public void resume();
 
+  /**
+   * Scrolls horizontally.
+   * @param unit  Amount to be scrolled.
+   */
   public void scrollHorizontal(int unit);
 
+  /**
+   * Scrolls vertically.
+   * @param unit  Amount to be scrolled
+   */
   public void scrollVertical(int unit);
 }
