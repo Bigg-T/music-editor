297e01f1a6783f400cc0a6d034b6fd6aa59c9c88
Thien <nguyen.thie@husky.neu.edu>
2016-12-02 16:07:36 -0500

    add more stuff

--- src/cs3500/music/model/ReadOnlyModel.java
+++ src/cs3500/music/model/ReadOnlyModel.java
@@ -1,7 +1,46 @@
 package cs3500.music.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by tiger on 12/2/16.
  */
-public class ReadOnlyModel {
+public class ReadOnlyModel implements ReadOnlyMusicModel {
+  private final IBasicMusicEditor<INote> model;
+
+  public ReadOnlyModel(IBasicMusicEditor<INote> model) {
+    this.model = model;
+  }
+
+  @Override
+  public int totalBeats() throws IllegalStateException {
+    return this.model.getLastBeat();
+  }
+
+  @Override
+  public List<Note> getNotes() throws IllegalStateException {
+    List<Note> notes = new ArrayList<>();
+    this.model.composition().values()
+            .forEach(x -> x.values()
+                    .forEach(y -> {
+                      notes.add((Note) y);
+                    }));
+    return notes;
+  }
+
+  @Override
+  public List<Note> getNotesAtBeat(int beat) throws IllegalStateException {
+    return null;
+  }
+
+  @Override
+  public int getBeatsPerMeasure() throws IllegalStateException {
+    return 0;
+  }
+
+  @Override
+  public int getTempo() {
+    return model.getTempo();
+  }
 }
