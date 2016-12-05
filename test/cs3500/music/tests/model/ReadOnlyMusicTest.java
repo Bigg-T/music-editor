272f0944c9ad10cccd7fd9331ce7fa7f61255c95
carneyrob <robert.edward.carney@gmail.com>
2016-12-05 17:35:53 -0500

    ReadOnlyModelTest

---
+++ test/cs3500/music/tests/model/ReadOnlyMusicTest.java
@@ -0,0 +1,51 @@
+package cs3500.music.model;
+
+
+import org.junit.Test;
+
+import java.util.SortedMap;
+
+import cs3500.music.model.BasicMusicEditor;
+import cs3500.music.model.ReadOnlyModel;
+import cs3500.music.model.ReadOnlyMusicModel;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * For the purposes of testing a read only model.
+ */
+public class ReadOnlyMusicTest {
+
+  ReadOnlyMusicModel model = new ReadOnlyModel(new
+          BasicMusicEditor.BasicCompositionBuilder().setTempo(120).addNote(1, 2, 1, 60, 3).
+          build());
+
+
+
+
+  @Test
+  public void testBPM()  {
+    assertEquals(4, model.getBeatsPerMeasure());
+  }
+
+  @Test
+  public void testGetNotes()  {
+    assertEquals(1, model.getNotes().size());
+  }
+
+  @Test
+  public void testGetNotesAt()  {
+    assertEquals(1, model.getNotesAtBeat(1).size());
+  }
+
+  @Test
+  public void testGetNotesAt2()  {
+    assertEquals("C 4", model.getNotesAtBeat(1).get(0).toString());
+  }
+
+  @Test
+  public void testGetTempo()  {
+    assertEquals(500000, model.getTempo());
+  }
+
+}
